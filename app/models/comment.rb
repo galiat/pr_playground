4b27d37406874f3ed4a2f6f1bc551616baacaa52
Galia <galiat@gmail.com>
2017-04-17 16:07:56 -0400

    to be dup’d

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,8 @@
 class Comment < ApplicationRecord
+  belongs_to :post
+  validates_presence_of :body
+
+  def determine_sentiment
+    SentimentAnalysis.new(sensitivity: 7).compute(body)
+  end
 end
