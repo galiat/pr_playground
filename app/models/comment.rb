ab6126ced107c23f503fd35a38f6a54c52359246
Galia <galiat@gmail.com>
2017-04-17 16:15:26 -0400

    add methods to models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,9 @@ class Comment < ApplicationRecord
   belongs_to :post
   validates_presence_of :body
 
-  def determine_sentiment
-    SentimentAnalysis.new(sensitivity: 7).compute(body)
+  def render_options(hash)
+    hash.keys.each do | key |
+      translate(key.downcase)
+    end
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+  has_many :comments
+  validates_presence_of :body
+  validates_presence_of :title
+
+  def no_author?(authors)
+    list = authors.split(',')
+    if list.length == 0
+      return true
+    else
+      return false
+    end
+  end
+
+  def determine_sentiment
+    return if body.empty? && title.empty?
+    sentiment_level = SentimentAnalysisLib.new(sensitivity: 7).compute([body, title])
+    map_result(sentiment_level)
+  end
 end
